97f58b1bd2edb6ecef0cbfea1c7a184214c724a3
yinyuan1990 <2802469196@qq.com>
2025-02-12 23:29:04 -0800

    Update OpenZeppelin库Ownable单一权限.sol
    1

--- OpenZeppelin库Ownable单一权限.sol
+++ OpenZeppelin库Ownable单一权限.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyContract is Ownable {
+    
+    uint256 public value;
+
+    constructor() Ownable(msg.sender){
+        
+    }
+
+    // 仅允许所有者更新值
+    function setValue(uint256 newValue) public onlyOwner {
+        value = newValue;
+    }
+    
+    // 所有者可以转移所有权
+    function transferOwnership(address newOwner) public override onlyOwner {
+       
+        _transferOwnership(newOwner);
+    }
+
+    
+
+}
