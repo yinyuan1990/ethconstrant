8addf7ff8a96931dd7cc38701b2603590abbfc0c
yinyuan1990 <2802469196@qq.com>
2025-02-12 23:23:45 -0800

    Update OpenZeppelin库重构代币合约.sol
    2

--- OpenZeppelin库重构代币合约.sol
+++ OpenZeppelin库重构代币合约.sol
@@ -1,1 +1,28 @@
 
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+// 导入 OpenZeppelin 的 ERC-20 实现
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract MyTokenERC is ERC20,ReentrancyGuard{
+
+
+    constructor(
+     string memory name,
+     string memory symbol,
+     uint256 num
+    ) ERC20(name,symbol){
+        _mint(msg.sender, num*10**decimals());
+        
+    }
+
+    function safeTransfer(address to,uint256 value) public nonReentrant {
+            _transfer(msg.sender, to, value);
+    } 
+    
+
+    
+}
