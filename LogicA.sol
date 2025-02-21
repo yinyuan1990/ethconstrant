f72032b6d5e0dd2d035509626bbd2293e357b4f7
yinyuan1990 <2802469196@qq.com>
2025-02-20 23:27:04 -0800

    Update LogicA.sol
    3

--- LogicA.sol
+++ LogicA.sol
@@ -1,1 +1,27 @@
 
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+
+contract LogicA {
+
+   
+    uint256 public  value;
+    
+
+    function setValue(uint256 newvalue) public payable {
+         value = newvalue;
+    }
+
+
+    function getCzc() public  pure  returns  (bytes32){
+
+       // 计算存储槽
+        bytes32 slot = bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1);
+               
+        return  slot;
+    } 
+
+
+}
