33125c4c8069eeea58f5a645abb02ef284719894
yinyuan1990 <2802469196@qq.com>
2025-02-20 23:27:27 -0800

    Update LogicB.sol
    3

--- LogicB.sol
+++ LogicB.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+
+contract LogicB {
+
+   
+    uint256 public  value;
+    
+    function setValue(uint256 newvalue) public payable {
+         value = newvalue*newvalue;
+    }
+
+     
+
+}
